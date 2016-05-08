710694793849440e23b8d2c0f4ae653ee635599e
fierceblonde <menitskaya@gmail.com>
2016-05-08 00:07:24 -0700

    add home page template

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000;
+
+// ==========================
+// middleware & config 
+// ==========================
+
+app.set('views', 'views');
+
+app.engine('hbs', exphbs ({
+extname: 'hbs',
+defaultLayout: 'main',
+layoutsDir: './views/layouts'
+}));
+app.set('view engine', 'hbs');
 
 // ==========================
 // routes
 // ==========================
 
 app.get("/", function(request, response) {
-	response.send('hi there');
+	response.render('home', {title: "My Site"});
 });
 
 //============================
